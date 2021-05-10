62b7dd8aa5694a1a73585c3cc677517408549e23
My-WZJ233 <3250383624@qq.com>
2021-05-10 17:25:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nuxvmz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nuxvmz)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('分享成功!');
+  }
+
+  onNotify() {
+    window.alert('商品价格低于700将通知您');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
